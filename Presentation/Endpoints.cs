e0cec02c087a5e6d7cf5b46be41633d58233a25f
exectly7 <iiburov7@icloud.com>
2026-01-20 20:03:29 +0300

    Template for email builder microservice

---
+++ Presentation/Endpoints.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UseCases;
+
+namespace Presentation;
+
+[ApiController]
+[Route("")]
+public class Endpoints : ControllerBase
+{
+    IUploadDataRequestHandler _uploadDataRequestHandler;
+    
+    public Endpoints(IUploadDataRequestHandler  uploadDataRequestHandler)
+    {
+        _uploadDataRequestHandler = uploadDataRequestHandler;
+    }
+    
+    [HttpPost("UploadData")]
+    public IActionResult UploadData([FromForm] UploadDataRequest request)
+    {
+        return Ok(_uploadDataRequestHandler.Handle(request));
+    }
+}
