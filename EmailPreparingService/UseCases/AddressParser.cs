7110dd97466b341bd2a353db664a3b9de61b1e48
exectly7 <iiburov7@icloud.com>
2026-01-21 14:47:57 +0300

    Added xlsx 1 row parsing and email valid checking

--- EmailPreparingService/UseCases/AddressParser.cs
+++ EmailPreparingService/UseCases/AddressParser.cs
@@ -1,4 +1,7 @@
+using System.Net.Mail;
 using Microsoft.AspNetCore.Http;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
 namespace UseCases;
 
@@ -6,6 +9,38 @@ public class AddressParser : IAddressParser
 {
     public List<string> Parse(IFormFile data)
     {
-        return ["email@mail.com"];
+        List<string> result = [];
+        using var stream = data.OpenReadStream();
+        IWorkbook workbook = new XSSFWorkbook(stream);
+        ISheet sheet = workbook.GetSheetAt(0);
+        IRow emails = sheet.GetRow(1);
+        for (int i = 0; i < emails.LastCellNum; i++)
+        {
+            ICell? cell = emails.GetCell(i);
+            if (cell == null)
+            {
+                break;
+            }
+            string value = cell.StringCellValue;
+            if (string.IsNullOrEmpty(value) || !isEmail(value))
+            {
+                break;
+            }
+            result.Add(value);
+        }
+        return result;
+    }
+
+    private bool isEmail(string value)
+    {
+        try
+        {
+            var email = new MailAddress(value);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
