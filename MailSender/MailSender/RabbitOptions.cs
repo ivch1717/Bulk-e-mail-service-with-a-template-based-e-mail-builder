a7eb707d7bd0ac0137f015a88315960a44050f54
pm4o <pmchurakov@edu.hse.ru>
2026-01-25 14:00:10 +0300

    initial mail sender added

---
+++ MailSender/MailSender/RabbitOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace MailSender;
+
+public sealed class RabbitOptions
+{
+    public string Host { get; init; } = "rabbitmq";
+    public int Port { get; init; } = 5672;
+    public string User { get; init; } = "guest";
+    public string Pass { get; init; } = "guest";
+
+    public string Exchange { get; init; } = "mail.send";
+    public string Queue { get; init; } = "mail.send.q";
+    public string RoutingKey { get; init; } = "send";
+    public ushort Prefetch { get; init; } = 10;
+    
+    public int Consumers { get; init; } = 5;
+}
