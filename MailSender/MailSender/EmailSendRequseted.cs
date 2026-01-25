3125852349f8efbc573857c7c0afa2a2cf504ac2
pm4o <pmchurakov@edu.hse.ru>
2026-01-25 14:00:10 +0300

    initial mail sender added

---
+++ MailSender/MailSender/EmailSendRequseted.cs
@@ -0,0 +1,10 @@
+﻿namespace MailSender;
+
+public sealed record EmailSendRequested(
+    Guid MessageId,
+    string To,
+    string Subject,
+    string HtmlBody,
+    string? FromEmail,
+    string? FromName
+);
