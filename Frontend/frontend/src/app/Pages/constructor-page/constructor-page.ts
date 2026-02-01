f7c18dc2b35316e6521ba4f2760324988379eb2c
ivch1717 <ivch1717@gmail.com>
2026-02-01 15:49:29 +0300

    add home page

---
+++ Frontend/frontend/src/app/Pages/constructor-page/constructor-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-constructor-page',
+  imports: [],
+  templateUrl: './constructor-page.html',
+  styleUrl: './constructor-page.css',
+})
+export class ConstructorPage {
+
+}
