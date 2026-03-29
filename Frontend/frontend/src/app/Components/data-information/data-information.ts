46fe2fe7652ecedcdd8796dad52510e87b76347b
exectly7 <iiburov7@icloud.com>
2026-03-29 13:23:22 +0300

    extractheadersfrontend + fixed duplicates in template variables

--- Frontend/frontend/src/app/Components/data-information/data-information.ts
+++ Frontend/frontend/src/app/Components/data-information/data-information.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-data-information',
@@ -6,7 +6,24 @@ import {Component, Input} from '@angular/core';
   templateUrl: './data-information.html',
   styleUrl: './data-information.css',
 })
-export class DataInformation {
+export class DataInformation implements OnChanges{
   @Input()
   variables: string[] = [];
+
+  @Input()
+  headers: string[] = [];
+
+  mapping: Map<string, string> = new Map<string, string>();
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.processMapping();
+  }
+
+  processMapping() {
+    for (let v of this.variables) {
+      if (this.headers.includes(v)) {
+        this.mapping.set(v, v);
+      }
+    }
+  }
 }
