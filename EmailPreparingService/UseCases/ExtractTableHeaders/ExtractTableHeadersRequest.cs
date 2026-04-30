8704f17021f2c70989960837ae066fd11037836d
exectly7 <iiburov7@icloud.com>
2026-04-30 17:38:47 +0300

    Small fixes

--- EmailPreparingService/UseCases/ExtractTableHeaders/ExtractTableHeadersRequest.cs
+++ EmailPreparingService/UseCases/ExtractTableHeaders/ExtractTableHeadersRequest.cs
@@ -3,9 +3,7 @@ using Microsoft.AspNetCore.Http;
 namespace UseCases.ExtractTableHeaders;
 
 /// <summary>
-/// Запрос для получения заголовков столбцов.
+/// Запрос для получения заголовков таблицы.
 /// </summary>
 /// <param name="table">Таблица с данными для подстановки.</param>
-public record ExtractTableHeadersRequest(
-    IFormFile table
-    );
+public record ExtractTableHeadersRequest(IFormFile table);
