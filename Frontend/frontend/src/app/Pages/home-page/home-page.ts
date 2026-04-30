38cc9f9209da655f6232cbe11ff0ff9aa0bc39c2
exectly7 <iiburov@proton.me>
2026-04-30 23:20:35 +0300

    Home page

--- Frontend/frontend/src/app/Pages/home-page/home-page.ts
+++ Frontend/frontend/src/app/Pages/home-page/home-page.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
-
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-home-page',
   imports: [
-    RouterLink
+    RouterLink,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule
   ],
   standalone: true,
   templateUrl: './home-page.html',
